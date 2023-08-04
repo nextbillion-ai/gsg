a0b50485f74c2d9a86a4131c0df22613c73dc664
chang <chang@nextbillion.ai>
2023-08-04 08:55:45 +0000

    fix: update .release.sh

--- .release.sh
+++ .release.sh
@@ -1,5 +1,7 @@
 set -e
 
+version=`go run main.go version | sed -e "s/gsg version //"`
+
 ./.build.sh
 go run main.go -m rsync -r -d build gs://static.nextbillion.io/tools/gsg/latest
-go run main.go -m rsync -r -d build gs://static.nextbillion.io/tools/gsg/$CI_COMMIT_TAG
+go run main.go -m rsync -r -d build gs://static.nextbillion.io/tools/gsg/$version
