220e287d8bbe6d597eaaee8f888e0e5df12905cf
Qiang FU <qiang@nextbillion.ai>
2023-08-31 17:08:00 +0800

    feat: rm deps of bar

--- .build.sh
+++ .build.sh
@@ -2,10 +2,10 @@ set -e
 
 rm -rf build
 
+# mv bar/bar.go bar/bar.go_backup && mv bar/bar.go_darwin_arm64 bar/bar.go
 mkdir -p build/darwin-arm64
-mv bar/bar.go bar/bar.go_backup && mv bar/bar.go_darwin_arm64 bar/bar.go
 GOOS=darwin GOARCH=arm64 go build -o build/darwin-arm64/gsg
-mv bar/bar.go bar/bar.go_darwin_arm64 && mv bar/bar.go_backup bar/bar.go
+# mv bar/bar.go bar/bar.go_darwin_arm64 && mv bar/bar.go_backup bar/bar.go
 
 mkdir -p build/darwin-amd64
 GOOS=darwin GOARCH=amd64 go build -o build/darwin-amd64/gsg
